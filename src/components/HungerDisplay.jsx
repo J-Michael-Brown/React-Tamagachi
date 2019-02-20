5ba89a70ee1645ba4947758f749085e77177f69b
Michael Brown and Ralph Perdomo <michael@>
2019-02-20 11:41:18 -0800

    accept props to display width percentage

--- src/components/HungerDisplay.jsx
+++ src/components/HungerDisplay.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import {STYLES} from '../styles';
 
-function HungerDisplay(){
+function HungerDisplay(props){
+
   return(
     <div style={STYLES.statBarOutter}>
-      <div style={{...STYLES.statBarInner, width: '50%'}}>
+      <div style={ {...STYLES.statBarInner, width: (props.hungerLevel+ '%')} }>
         <span style={{color: 'green'}}>Hunger</span>
         &nbsp;
       </div>
     </div>
   );
 }
+
+HungerDisplay.propTypes = {
+  hungerLevel: PropTypes.number
+};
 
 export default HungerDisplay;
